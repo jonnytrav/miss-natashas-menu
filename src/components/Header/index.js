ec4c2ca424fc6d674e255861b64061c2288cc49e
Jonathan Traverso <jtraverso7@gmail.com>
2020-07-20 17:48:18 -0400

    Set up BrowserRouter and changed folder structure.

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <nav>
+        <a>Home</a>
+        <a>Menu</a>
+        <a>About</a>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
