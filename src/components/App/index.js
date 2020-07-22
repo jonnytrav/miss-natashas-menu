add9be539be3bd25a9ff173eb2b94537989529aa
Jonathan Traverso <jtraverso7@gmail.com>
2020-07-21 21:33:49 -0400

    Wrote MUI theme boilerplate and dislpayed new styling changes.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { Route } from "react-router-dom";
 
+import { MuiThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import theme from "../../theme.js";
+
 import Header from "../Header";
 import LandingPage from "../LandingPage";
 import Menu from "../Menu";
@@ -9,12 +13,15 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Route path="/" component={Header}></Route>
-      <Route path="/home" component={LandingPage}></Route>
-      <Route path="/menu" component={Menu}></Route>
-      <Route path="/about" component={About}></Route>
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className="App">
+        <Route path="/" component={Header}></Route>
+        <Route path="/home" component={LandingPage}></Route>
+        <Route path="/menu" component={Menu}></Route>
+        <Route path="/about" component={About}></Route>
+      </div>
+    </MuiThemeProvider>
   );
 }
 
---
+++ src/theme.js
@@ -0,0 +1,11 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#44318d"
+    }
+  }
+});
+
+export default theme;
