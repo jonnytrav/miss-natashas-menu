e4cfdc1d082387f14368b91501f630b5ed711ead
Jonathan Traverso <jtraverso7@gmail.com>
2020-07-25 13:06:37 -0400

    Removed Material UI. Pushing WIP.

--- src/components/App/appStyling.js
+++ src/components/App/appStyling.js
@@ -3,7 +3,10 @@ import Image from "../../Images/purple-pink-sky-unsplash.jpg";
 
 const useStyles = makeStyles({
   wrapper: {
-    backgroundImage: `url(${Image})`
+    backgroundImage: `url(${Image})`,
+    zIndex: -1,
+    height: "100%",
+    width: "100%"
   }
 });
 
--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Route } from "react-router-dom";
 
-import { MuiThemeProvider } from "@material-ui/core/styles";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import theme from "../../themes/theme.js";
+// import { MuiThemeProvider } from "@material-ui/core/styles";
+// import CssBaseline from "@material-ui/core/CssBaseline";
+// import theme from "../../themes/theme.js";
 import useStyles from "./appStyling.js";
 
 import Header from "../Header";
@@ -15,15 +15,12 @@ function App() {
   const classes = useStyles();
 
   return (
-    <MuiThemeProvider theme={theme}>
-      <CssBaseline />
-      <div className={classes.appWrapper}>
-        <Route path="/" component={Header}></Route>
-        <Route path="/home" component={LandingPage}></Route>
-        <Route path="/menu" component={Menu}></Route>
-        <Route path="/about" component={About}></Route>
-      </div>
-    </MuiThemeProvider>
+    <div className={classes.wrapper}>
+      <Route path="/" component={Header}></Route>
+      <Route path="/home" component={LandingPage}></Route>
+      <Route path="/menu" component={Menu}></Route>
+      <Route path="/about" component={About}></Route>
+    </div>
   );
 }
 
