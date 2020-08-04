5db1b26e051baca5de0528ed770e7d5461379f7d
Jonathan Traverso <jtraverso7@gmail.com>
2020-08-03 21:45:08 -0400

    Changed main background image and some header styling.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -9,31 +9,34 @@ const Header = props => {
   // };
 
   return (
-    <div>
-      <div className="border-2 border-white w-2/3 h-64 mx-auto pt-10 bg-crinkled bg-cover bg-center rounded-md relative z-0">
-        <div className="flex flex-row justify-center">
-          <Link
-            to="/home"
-            // *********** look into "active" utility class later *************
-            className="rounded-md border border-purple-800 text-white mx-10 my-10 px-2 py-2 hover:border-purple-400 shadow-lg"
-          >
-            <a>Home</a>
-          </Link>
+    <div className="border-2 border-white w-2/3 h-64 mx-auto pt-10 bg-crinkled bg-cover bg-center rounded-md relative z-0">
+      <div className="flex flex-row justify-center">
+        <Link
+          to="/home"
+          // *********** look into "active" utility class later *************
+          className="rounded-md border border-purple-800 text-white mx-10 my-10 px-2 py-2 hover:border-purple-400 shadow-lg"
+        >
+          <a>Home</a>
+        </Link>
 
-          <Link
-            to="/menu"
-            className="rounded-md border border-purple-800 text-white mx-10 my-10 px-2 py-2 hover:border-purple-400 shadow-lg"
-          >
-            <a>Menu</a>
-          </Link>
+        <Link
+          to="/menu"
+          className="rounded-md border border-purple-800 text-white mx-10 my-10 px-2 py-2 hover:border-purple-400 shadow-lg"
+        >
+          <a>Menu</a>
+        </Link>
 
-          <Link
-            to="/about"
-            className="rounded-md border border-purple-800 text-white mx-10 my-10 px-2 py-2 hover:border-purple-400 shadow-lg"
-          >
-            <a>About</a>
-          </Link>
-        </div>
+        <Link
+          to="/about"
+          className="rounded-md border border-purple-800 text-white mx-10 my-10 px-2 py-2 hover:border-purple-400 shadow-lg"
+        >
+          <a>About</a>
+        </Link>
+      </div>
+      <div className="flex flex-row justify-center">
+        <h1 className="text-5xl text-white font-thick-cursive mx-auto">
+          Miss Tasha's Menu
+        </h1>
       </div>
     </div>
   );
