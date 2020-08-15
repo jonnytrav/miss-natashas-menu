407254e74ed6415220205f8a32241a06f06bc214
Jonathan Traverso <jtraverso7@gmail.com>
2020-08-15 15:29:20 -0400

    Scratched lazy loading idea, scaffolding carousel instead. Pushing WIP.

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -2,15 +2,7 @@ import React from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 
 const Gallery = () => {
-  return (
-    <div className="border border-red-600">
-      <LazyLoadImage
-        src="../../images/gallery/FullSizeRender 4.jpeg"
-        className="w-full h-screen"
-        height={500}
-      />
-    </div>
-  );
+  return <div className="border border-red-600"></div>;
 };
 
 export default Gallery;
