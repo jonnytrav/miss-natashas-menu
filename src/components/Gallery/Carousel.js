c947e30fbf4f59bd16db265eb7cb16ca37a22ceb
Jonathan Traverso <jtraverso7@gmail.com>
2020-08-19 21:13:27 -0400

    Nav buttons displaying for Gallery carousel component but not pics rendered. WIP.

--- src/components/Gallery/Carousel.js
+++ src/components/Gallery/Carousel.js
@@ -1,17 +1,27 @@
 import React, { useState } from "react";
-import Carousel, { Dots } from "@brainhubeu/react-carousel";
-import "@brainhubeu/react-carousel/lib/style.css";
+// import Carousel, { Dots } from "@brainhubeu/react-carousel"
+// import "@brainhubeu/react-carousel/lib/style.css";
+import { Slide } from "react-slideshow-image";
+import "react-slideshow-image/dist/styles.css";
 
-import Pic1 from "../../images/gallery/FullSizeRender4.jpeg";
+// import Pic1 from "../../images/gallery/FullSizeRender4.jpeg";
+
+const galleryImages = [
+  "../../images/gallery/FullSizeRender4.jpeg",
+  "../../images/gallery/FullSizeRender5.jpeg"
+];
 
 const GalleryCarousel = () => {
   return (
     <div>
-      <Carousel className="">
-        <image src={Pic1} alt="first pic" />
-        {/* <image src={} />
-        <image src={} /> */}
-      </Carousel>
+      <Slide>
+        <div className="each-slide w-full">
+          <div style={{ backgroundImage: `url(${galleryImages[0]})` }}></div>
+        </div>
+        <div className="each-slide">
+          <div style={{ backgroundImage: `url(${galleryImages[1]})` }}></div>
+        </div>
+      </Slide>
     </div>
   );
 };
