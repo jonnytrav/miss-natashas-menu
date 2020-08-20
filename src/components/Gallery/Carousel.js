438ed6f19b100f1d418f3f1bef991b4c6e9a9f14
Jonathan Traverso <jtraverso7@gmail.com>
2020-08-19 20:54:12 -0400

    Added BrainHub Carousel component. Pushing for debugging purposes.

--- src/components/Gallery/Carousel.js
+++ src/components/Gallery/Carousel.js
@@ -1,8 +1,19 @@
 import React, { useState } from "react";
+import Carousel, { Dots } from "@brainhubeu/react-carousel";
+import "@brainhubeu/react-carousel/lib/style.css";
 
-const Carousel = () => {
-  const [current, setCurrent] = useState(0);
-  return <div></div>;
+import Pic1 from "../../images/gallery/FullSizeRender4.jpeg";
+
+const GalleryCarousel = () => {
+  return (
+    <div>
+      <Carousel className="">
+        <image src={Pic1} alt="first pic" />
+        {/* <image src={} />
+        <image src={} /> */}
+      </Carousel>
+    </div>
+  );
 };
 
-export default Carousel;
+export default GalleryCarousel;
--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,8 +1,12 @@
 import React from "react";
-import { LazyLoadImage } from "react-lazy-load-image-component";
+import GalleryCarousel from "./Carousel.js";
 
 const Gallery = () => {
-  return <div className="border border-red-600"></div>;
+  return (
+    <div className="border border-red-600 h-full">
+      <GalleryCarousel />
+    </div>
+  );
 };
 
 export default Gallery;
