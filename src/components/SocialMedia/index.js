599962db0f223937995d937f4ca288ad966c9f1f
Jonathan Traverso <jtraverso7@gmail.com>
2020-08-28 23:44:59 -0400

    Pushing logo issue for debugging.

--- src/components/SocialMedia/index.js
+++ src/components/SocialMedia/index.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { twitter } from "../../images/icons/twitter-logo.eps";
 
 const SocialMedia = () => {
   return (
-    <div>
+    <div className="h-full">
       <div>Icons for various platforms would go here.</div>
+      <div className="h-64 border border-red-600">
+        <img src={twitter} className="h-full" />
+      </div>
     </div>
   );
 };
