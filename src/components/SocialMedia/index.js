4804f835452a6002175040590f3c7a2ca88e798f
Jonathan Traverso <jtraverso7@gmail.com>
2020-08-29 15:02:51 -0400

    All social media icons properly displayed.

--- src/components/SocialMedia/index.js
+++ src/components/SocialMedia/index.js
@@ -2,14 +2,10 @@ import React from "react";
 
 const SocialMedia = () => {
   return (
-    <div className="max-height-2/3 h-full">
-      <div>Icons for various platforms would go here.</div>
-      <div className="twitter-logo h-32 w-32 bg-no-repeat bg-center">
-        {/* <img
-        className="h-24 w-24"
-        src="../../images/icons/Twitter_Logo_Blue.svg"
-      /> */}
-      </div>
+    <div className="max-height-2/3 h-full flex justify-evenly">
+      <div className="twitter-logo h-32 w-32 bg-no-repeat bg-center "></div>
+      <div className="facebook-logo h-32 w-32 bg-no-repeat bg-center "></div>
+      <div className="instagram-logo h-32 w-32 bg-no-repeat bg-center "></div>
     </div>
   );
 };
